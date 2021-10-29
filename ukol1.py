9b61aeb7a5e91afd3d20b669982498add3ffc258
Pepa Zátka <zatkaj@natur.cuni.cz>
2021-10-29 21:55:46 +0200

    Opravy issues

--- ukol1.py
+++ ukol1.py
@@ -1,4 +1,7 @@
-from turtle import forward, right, left, textinput, numinput, title, speed, color, goto, penup, pendown, pensize, circle
+from turtle import exitonclick, forward, right, left, textinput, numinput, title, speed, color, goto, penup, pendown, pensize, circle
+
+VELIKOST_POLE = 25   #Konstanta určující velikost pole mřížky.
+
 
 # Název okna:
 title("Pišqworky od Pepy verze 1.1")
@@ -7,102 +10,92 @@ title("Pišqworky od Pepy verze 1.1")
 hrac1 = textinput("Začátek hry!","Zadej jméno prvního hráče:")
 
 # Kontrola vstupu. Pokud uživatel potvrdí jméno bez zadání znaku, bude vyzván k opětovnému zadání:
-while True:
-        
-    if hrac1 == "" :        
-        hrac1 = textinput("Chyba","Chyba, zadej platné jméno prvního hráče:")
-        
-        continue
-    else:
-        break
+
+while hrac1 == "":        
+    hrac1 = textinput("Chyba","Chyba, zadej platné jméno prvního hráče:")
+
 
 hrac2 = textinput("Začátek hry!","Zadej jméno druhého hráče:")
 
 # Kontrola vstupu. Pokud uživatel potvrdí jméno bez zadání znaku, či se bude jméno shodovat s prvním, bude vyzván k opětovnému zadání:
-while True:
-        
-    if hrac2 == "" or hrac1 == hrac2 : 
-        hrac2 = textinput("Chyba","Chyba, zadej platné jméno druhého hráče:")
-        
-        continue
-    else:
-        break
+
+while hrac2 == "" or hrac1 == hrac2 :        
+    hrac2 = textinput("Chyba","Chyba, zadej platné jméno druhého hráče:")
 
 # Nastavení mřížky:
 
-mrizka_x = numinput("Začátek hry!","Na mřížce o kolika polích byste chtěli hrát?")
+mrizka_x = numinput("Začátek hry!","Zadejte rozměr x hrací mřížky:")
+mrizka_x_kontrola  = mrizka_x % 1 
 
-while True:
+while mrizka_x < 3 or mrizka_x_kontrola != 0: #Zbytek z celočíselného dělení, kontoluji, jestli je zbytkem nula, jinak bylo zadáno desetinné číslo = neplatný rozměr mřížky.
 
-    mrizka_kontrola  = mrizka_x % 1 #Zbytek z celočíselného dělení, v podmínce níže, kontoluji, jestli je zbytkem nula, jinak bylo zadáno desetinné číslo = neplatný rozměr mřížky.
-        
-    if mrizka_x < 3 or mrizka_x > 20 or mrizka_kontrola != 0:
-        mrizka_x = numinput("Chyba","Chyba, zadejte celočíselný rozměr mezi 3 a 20:")
-        
-        continue
-    else:
-        break
-
+    mrizka_x = numinput("Chyba","Chyba, zadejte celočíselný rozměr x hrací mřížky, větší než 2:")
+    mrizka_x_kontrola  = mrizka_x % 1 
 
 mrizka_x = int(mrizka_x)
 
+
+mrizka_y = numinput("Začátek hry!","Zadejte rozměr y hrací mřížky:")
+
+mrizka_y_kontrola  = mrizka_y % 1 
+
+while mrizka_y < 3 or mrizka_y_kontrola != 0: #Zbytek z celočíselného dělení, kontoluji, jestli je zbytkem nula, jinak bylo zadáno desetinné číslo = neplatný rozměr mřížky.
+
+    mrizka_y = numinput("Chyba","Chyba, zadejte celočíselný rozměr y hrací mřížky, větší než 2:")
+    mrizka_y_kontrola  = mrizka_y % 1 
+
+mrizka_y = int(mrizka_y)
+
+
 # Vykreslení mřížky
 
-k = 25   #Konstanta určující velikost pole mřížky.
-
 speed(0)
-for _ in range(mrizka_x):
+for _ in range(mrizka_y):
     for _ in range(mrizka_x):
         for _ in range(4):
-            forward(k)
+            forward(VELIKOST_POLE)
             left(90)
-        forward(k)
+        forward(VELIKOST_POLE)
 
     penup()
     left(180)
-    forward(k * mrizka_x)
+    forward(VELIKOST_POLE * mrizka_x)
     right(90)
-    forward(k)
+    forward(VELIKOST_POLE)
     right(90)
     pendown()
 
 # Podmínka, která ošetřuje různost dalšího cyklu v závislosti na počtu polí mřížky. Je-li počet polí lichý, int zaokrouhlí dolů a tak musím přičíst 1. 
-# Zároveň ale přičítám k oběma variantám 1, aby vhodně fungovala fce range. Tzn. k sudým +1, k lichým +2.
+# Zároveň ale přičítám k oběma variantám 1, aby vhodně fungovala fce range. Tzn. dohromady k sudým +1, k lichým +2.
 
-if mrizka_x%2 == 0:
-    pocet_tahu = int((mrizka_x*mrizka_x)/2+1)
-elif mrizka_x%2 != 0:
-    m = int((mrizka_x*mrizka_x)/2)
+if      (mrizka_x * mrizka_y) % 2 == 0:
+    pocet_tahu = int((mrizka_x * mrizka_y)/2+1)
+elif    (mrizka_x * mrizka_y) % 2 != 0:
+    m = int((mrizka_x * mrizka_y)/2)
     pocet_tahu = int(m+2)
 
 for i in range(1, pocet_tahu):         # Cyklus zajišťující konec hry po zaplnění hracího pole
 
     # Sběr souřadnic:
 
-    Sour_x = numinput(f"Tah {i} hráče {hrac1}", f"{hrac1}, zvol souřadnici x svého tahu:")
+    sour_x = numinput(f"Tah {i} hráče {hrac1}", f"{hrac1}, zvol souřadnici x svého tahu:")
 
-    while True:
+    #Kontrola vstupu, prázdný vstup None je ošetřen defaultně
+    while sour_x < 1 or sour_x > mrizka_x or sour_x % 1 != 0 :
             
-        if Sour_x < 1 or Sour_x > mrizka_x or Sour_x%1 != 0 :         #Kontrola vstupu, prázdný vstup None je ošetřen defaultně
-            Sour_x = numinput(f"Tah {i} hráče {hrac1}","Chybná souřadnice x, zadej platnou:")
-            
-            continue
-        else:
-            break
+        sour_x = numinput(f"Tah {i} hráče {hrac1}","Chybná souřadnice x, zadej platnou:")
 
-    Sour_y = numinput(f"Tah {i} hráče {hrac1}", f"{hrac1}, zvol souřadnici y svého tahu:")
 
-    while True:
-            
-        if Sour_y < 1 or Sour_y > mrizka_x or Sour_y%1 != 0 :        # Kontrola vstupu, prázdný vstup None je ošetřen defaultně
-            Sour_y = numinput(f"Tah {i} hráče {hrac1}","Chybná souřadnice y, zadej platnou:")
-            
-            continue
-        else:
-            break
+    sour_y = numinput(f"Tah {i} hráče {hrac1}", f"{hrac1}, zvol souřadnici y svého tahu:")
 
-    x = Sour_x * k - k        # Jedno políčko má k pixelů, proto násobím. Odčítám pro to, aby kreslení začínalo v levém dolním rohu (pro 1,1 pixel 0,0)
-    y = Sour_y * k - k
+    while sour_y < 1 or sour_y > mrizka_x or sour_y % 1 != 0 :
+            
+        
+        sour_y = numinput(f"Tah {i} hráče {hrac1}","Chybná souřadnice y, zadej platnou:")
+            
+
+    x = sour_x * VELIKOST_POLE - VELIKOST_POLE        # Jedno políčko má k pixelů, proto násobím. Odčítám pro to, aby kreslení začínalo v levém dolním rohu (pro 1,1 pixel 0,0)
+    y = sour_y * VELIKOST_POLE - VELIKOST_POLE
 
 
     # kreslení křížku na zadaných souřadnicích:
@@ -111,42 +104,35 @@ for i in range(1, pocet_tahu):         # Cyklus zajišťující konec hry po zap
     pendown()
     pensize(2)
     color("blue")
-    goto([x+k,y+k])
+    goto([x+VELIKOST_POLE,y+VELIKOST_POLE])
     penup()
-    goto([x+k,y])
+    goto([x+VELIKOST_POLE,y])
     pendown()
-    goto([x,y+k])
+    goto([x,y+VELIKOST_POLE])
 
-    if i+1 == pocet_tahu and mrizka_x%2 != 0:   # Hraje-li se na lichý počet polí, program zde v posledním kole zajistí, aby nemohl hrát hráč 2, který už by neměl volné pole.
+    if i+1 == pocet_tahu and mrizka_x*mrizka_y%2 != 0:   # Hraje-li se na lichý počet polí, program zde v posledním kole zajistí, aby nemohl hrát hráč 2, který už by neměl volné pole.
         break
 
 
 
-    Sour2_x = numinput(f"Tah {i} hráče {hrac2}", f"{hrac2}, zvol souřadnici x svého tahu:")
+    sour2_x = numinput(f"Tah {i} hráče {hrac2}", f"{hrac2}, zvol souřadnici x svého tahu:")
 
-    while True:
-            
-        if Sour2_x < 1 or Sour2_x > mrizka_x or Sour2_x % 1 != 0 :      #Kontrola vstupu, prázdný vstup None je ošetřen samotnou fcí numinput
-            Sour2_x = numinput(f"Tah {i} hráče {hrac2}","Chybná souřadnice x, zadej platnou:")
-            
-            continue
-        else:
-            break
+    #Kontrola vstupu, prázdný vstup None je ošetřen samotnou fcí numinput
+    while sour2_x < 1 or sour2_x > mrizka_x or sour2_x % 1 != 0 :
 
-    Sour2_y = numinput(f"Tah {i} hráče {hrac2}", f"{hrac2}, zvol souřadnici y svého tahu:")
-
-    while True:
+        sour2_x = numinput(f"Tah {i} hráče {hrac2}","Chybná souřadnice x, zadej platnou:")
             
-        if Sour2_y < 1 or Sour2_y > mrizka_x or Sour2_y % 1 != 0 :      #Kontrola vstupu, prázdný vstup None je ošetřen samotnou fcí numinput
-            Sour2_y = numinput(f"Tah {i} hráče {hrac2}","Chybná souřadnice y, zadej platnou:")   
-                
-            continue
-        else:
-            break
+
+    sour2_y = numinput(f"Tah {i} hráče {hrac2}", f"{hrac2}, zvol souřadnici y svého tahu:")
+
+    #Kontrola vstupu, prázdný vstup None je ošetřen samotnou fcí numinput
+    while sour2_y < 1 or sour2_y > mrizka_y or sour2_y % 1 != 0 :
+        
+        sour2_y = numinput(f"Tah {i} hráče {hrac2}","Chybná souřadnice y, zadej platnou:")   
 
 # Úprava souřadnic tak, aby kolečko bylo vykresleno uvnitř zvoleného pole mřížky:
-    x2 = Sour2_x * k - (k/2)    # Násobení k <- jedno pole má k pixelů. Minus (k/2) -> Dostane se doprostřed pole na x-ové ose
-    y2 = Sour2_y * k - (0.9*k)  # Násobení k <- jedno pole má k pixelů. Minus (0,9k) -> Dostane se do spodní části pole na y-ové ose
+    x2 = sour2_x * VELIKOST_POLE - (VELIKOST_POLE / 2)    # Násobení k <- jedno pole má k pixelů. Minus (k/2) -> Dostane se doprostřed pole na x-ové ose
+    y2 = sour2_y * VELIKOST_POLE - (0.9 * VELIKOST_POLE)  # Násobení k <- jedno pole má k pixelů. Minus (0,9k) -> Dostane se do spodní části pole na y-ové ose
 
 
     # kreslení kolečka na zadaných souřadnicích:
@@ -155,7 +141,7 @@ for i in range(1, pocet_tahu):         # Cyklus zajišťující konec hry po zap
     pendown()
     pensize(2)
     color("red")
-    circle(0.4*k,360)
+    circle(0.4*VELIKOST_POLE,360)
 
-
+exitonclick()
 print(f"Děkuji, že jste hráli Pišqworky od Pepy, {hrac1} a {hrac2}! Gratuluji vítězi.")
