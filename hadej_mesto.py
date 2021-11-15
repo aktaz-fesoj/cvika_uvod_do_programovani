024ab58c0a3f9513ee59bbde490c351922502df5
Pepa Zátka <zatkaj@natur.cuni.cz>
2021-11-15 17:14:27 +0100

    uprava

--- hadej_mesto.py
+++ hadej_mesto.py
@@ -2,20 +2,32 @@
 import random
 
 mesto = random.choice(["Praha", "Brno", "Kolín", "Lípa", "Rumburk", "Jestřebí", "Pacov"])
-pismeno = input("Zadej písmeno:")
-
+mesto = mesto.lower()
 mesto_list = list(mesto)
 delka_slova = len(mesto_list)
-print(mesto_list)
 
 Hadane = []
 for i in range(delka_slova):
-    Hadane.extend("_")
-for i in range(delka_slova):
-    if pismeno in mesto_list[i]:
-        Hadane[i] = pismeno
+        Hadane.extend("_")
 
-print (*Hadane, sep = "")   #TOJEONO
+while mesto_list != Hadane:
+    pismeno = input("Zadej písmeno:")
+
+    for i in range(delka_slova):
+        if pismeno in mesto_list[i]:
+            if i == 0:
+                Hadane[i] = pismeno.upper()
+            else:
+                Hadane[i] = pismeno
+    if pismeno in mesto_list:
+        print("Trefa!")
+        print (*Hadane, sep = " ")   #TOJEONO
+    else:
+        print("To není ono :(")
+
+print("Je to doma! Dokázal jsi uhodnout hledané město!")
+
+
 
 
             
